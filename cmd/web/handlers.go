c4d9ee1c10f03e244873dc3a1ab70043f1d7875e
Achmad Alfan Ahsani <achmadalfanahsani@gmail.com>
2023-10-27 22:08:10 +0700

    parsing form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -55,9 +55,22 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mountain Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	expires := 7
+	r.Body = http.MaxBytesReader(w, r.Body, 4096)
+
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
 	id, err := app.Snippets.Insert(title, content, expires)
 	if err != nil {
